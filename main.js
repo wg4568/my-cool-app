f7a2dbcf05de7535dfec160177210c8c12a1663b
Alexander Goldberg <alexgberg@berkeley.edu>
2024-03-24 14:15:58 -0700

    Added env file stuff that william wanted

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
 const port = 3000
 
 app.get('/flag', (req, res) => {
-  // TODO: get flag from environment file
-  res.send('Not implemented yet')
+  console.log('Getting flag')
+  console.log(process.env.FLAG)
+  res.send("blu{"+process.env.FLAG+"}")
 })
 
 app.listen(port, () => {
